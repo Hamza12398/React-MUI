abd4e838a007b46c232b5032badd4a66a76c2cae
Hamza12398 <hamzacherkaoui806@gmail.com>
2024-02-23 17:17:36 +0100

    master ui

--- todo-list/src/MaterUi.jsx
+++ todo-list/src/MaterUi.jsx
@@ -1,12 +1,10 @@
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 export default function MaterUi() {
   return (
-    <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </Stack>
+    <Button variant="contained" disableElevation>
+      Disable elevation
+    </Button>
   );
 }
